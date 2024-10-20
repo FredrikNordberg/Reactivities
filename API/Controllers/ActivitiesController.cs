549c82acf2d435931d57f2993e03921e4ee886d7
Fredrik Nordberg <fredrik.nordberg@hotmail.com>
2024-10-20 15:50:37 +0200

    third commit

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,6 @@
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -7,30 +9,45 @@ namespace API.Controllers
 {
     public class ActivitiesController : BaseApiController
     {
-        private readonly DataContext _context;
-        
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-           
-            
-        }
 
         [HttpGet] //api/activities
         public async Task<ActionResult<List<Activity>>> GetActvities()
         {
-            return await _context.Activities.ToListAsync();
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] //api/activities/fef2f2323f23f
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
-{
-    var activity = await _context.Activities.FindAsync(id);
+        {
+            return await Mediator.Send(new Details.Query { Id = id });
+        }
 
-    if (activity == null) 
-        return NotFound();
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity)
+        {
+            await Mediator.Send(new Create.Command { Activity = activity });
+            return Ok();
+        }
 
-    return Ok(activity); // Explicitly wrapping it in Ok() to avoid null argument warning
-}
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            await Mediator.Send(new Edit.Command { Activity = activity });
+            return Ok();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)   
+        {
+            await Mediator.Send(new Delete.Command {Id = id});
+            
+            return Ok();
+        }
+
+
+
     }
 }
